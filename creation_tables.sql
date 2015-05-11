994a59b50349a2301de23d657da546358a7aa32c
willemotlucas <willemotlucas@gmail.com>
2015-05-11 09:52:39 +0200

    correction of creation ables file and drop tables file

--- creation_tables.sql
+++ creation_tables.sql
@@ -1,23 +1,24 @@
 CREATE TABLE Editeur (
-id 			INTEGER(4) 			PRIMARY KEY,
-nom 		VARCHAR(20), 
-contact 	VARCHAR(20),
-url 		VARCHAR(30),
-UNIQUE NOT NULL (nom, contact, url)
+id 			INTEGER				PRIMARY KEY,
+nom 		VARCHAR(20)			NOT NULL, 
+contact 	VARCHAR(20)			NOT NULL,
+url 		VARCHAR(30)			NOT NULL,
+UNIQUE(nom, contact, url)
 );
 
 CREATE TABLE Utilisateur (
-idClient 	INTEGER(4) 			PRIMARY KEY,
-nom 		VARCHAR(20), 
-UNIQUE NOT NULL (nom, prénom)
+idClient 	INTEGER 			PRIMARY KEY,
+nom 		VARCHAR(20)			NOT NULL, 
+prenom 		VARCHAR(20)			NOT NULL,
+UNIQUE(nom, prenom)
 );
 
 CREATE TABLE CartePrepayee (
 numero 			VARCHAR(16) 		PRIMARY KEY,
 dateExpiration 	DATE 				NOT NULL, 
-montantDepart 	REAL(6) 			NOT NULL,
-montantCourant 	REAL(6),
-client 			INTEGER(2) 			REFERENCES Editeur(id)
+montantDepart 	REAL 				NOT NULL,
+montantCourant 	REAL,
+client 			INTEGER 			REFERENCES Editeur(id)
 );
 
 --Contrainte : PROJ(CartePrépayée,client)=PROJ(Utilisateur,IdClient)
@@ -25,30 +26,29 @@ client 			INTEGER(2) 			REFERENCES Editeur(id)
 CREATE TABLE CarteBleue ( 
 numero 			VARCHAR(16) 		PRIMARY KEY,
 dateExpiration 	DATE 				NOT NULL, 
-cryptogramme 	INTEGER(3)			NOT NULL
+cryptogramme 	INTEGER 			NOT NULL
 );
 
 CREATE TABLE Transaction (
-id 				INTEGER 			PRIMARY KEY,
-montant 		REAL(6)				NOT NULL,
-acheteur 		INTEGER(4) 			REFERENCES Utilisateur(idClient) NOT NULL,
-destinataire 	INTEGER(4) 			REFERENCES Utilisateur(idClient) NOT NULL,
-carte 			INTEGER(10) 		REFERENCES CartePrepayee(numero),
-CB 				INTEGER(10) 		REFERENCES CarteBleue(numero),
+id 				INTEGER 				PRIMARY KEY,
+montant 		REAL					NOT NULL,
+acheteur 		INTEGER					REFERENCES Utilisateur(idClient) NOT NULL,
+destinataire 	INTEGER					REFERENCES Utilisateur(idClient) NOT NULL,
+carte 			VARCHAR(16) 			REFERENCES CartePrepayee(numero),
+CB 				VARCHAR(16) 			REFERENCES CarteBleue(numero),
 CHECK (carte IS NOT NULL OR CB IS NOT NULL)
 );
 
 
 CREATE TABLE Application (
 nom 			VARCHAR(15) 		PRIMARY KEY,
-editeur 		INTEGER(4) 			REFERENCES Editeur(id) NOT NULL,
+editeur 		INTEGER				REFERENCES Editeur(id) NOT NULL,
 description 	VARCHAR 
 );
---est ce la peine de spécifier le NOT NULL quand clé étrangère ??
 
 CREATE TABLE Ressource (
 nom 			VARCHAR(15) 		PRIMARY KEY,
-editeur 		INTEGER(4) 			REFERENCES Editeur(id) NOT NULL,
+editeur 		INTEGER				REFERENCES Editeur(id) NOT NULL,
 app 			VARCHAR(15) 		REFERENCES Application(nom) NOT NULL,
 description 	VARCHAR
 );
@@ -63,74 +63,73 @@ PRIMARY KEY(ressource, achat)
 
 CREATE TABLE Achat_simple_app (
 app 			VARCHAR(15) 		REFERENCES Application(nom),
-achat 			INTEGER				REFERENCES Transaction(id),
+achat 			INTEGER 			REFERENCES Transaction(id),
 dateAchat 		DATE 				NOT NULL,
 PRIMARY KEY(app, achat)
 );
 
 CREATE TABLE Avis (
-client 			INTEGER(4) 			REFERENCES Utilisateur(idClient),
+client 			INTEGER				REFERENCES Utilisateur(idClient),
 app 			VARCHAR(15) 		REFERENCES Application(nom),
-note 			INTEGER(1),
+note 			INTEGER				CHECK(note > 0 AND note < 6),
 commentaire 	VARCHAR(500),
 PRIMARY KEY(client,app)
---CHECK (note >1 AND note <5),
 );
 
 CREATE TABLE Abonnement (
 app 			VARCHAR(15) 		REFERENCES Application(nom),
-achat 			INTEGER				REFERENCES Transaction(id),
+achat 			INTEGER 			REFERENCES Transaction(id),
 automatique 	BOOLEAN,
-nbMois 			INTEGER(2),
-prixAbonnement 	REAL(6),
+nbMois 			INTEGER,
+prixAbonnement 	REAL,
 dateAbonnement 	DATE,
-PRIMARY KEY(achat,app),
-CHECK(nbMois NOT NULL if automatique = FALSE)
+PRIMARY KEY(achat,app)
+--CHECK(nbMois NOT NULL if automatique = FALSE)
 );
 
 CREATE TABLE OS (
-id 				INTEGER(2) PRIMARY KEY,
-constructeur 	VARCHAR(20),
-version 		VARCHAR(10),
-UNIQUE NOT NULL (constructeur, version)
+id 				INTEGER 			PRIMARY KEY,
+constructeur 	VARCHAR(20)			NOT NULL,
+version 		VARCHAR(10)			NOT NULL,
+UNIQUE(constructeur, version)
 );
 
 CREATE TABLE Modele (
-id 				INTEGER(4) 			PRIMARY KEY,
-constructeur 	VARCHAR(20),
-designation 	VARCHAR(20),
-systeme 		INTEGER(2) 			REFERENCES OS(id) NOT NULL,
-UNIQUE NOT NULL (constructeur, designation)
+id 				INTEGER				PRIMARY KEY,
+constructeur 	VARCHAR(20)			NOT NULL,
+designation 	VARCHAR(20)			NOT NULL,
+systeme 		INTEGER				REFERENCES OS(id) NOT NULL,
+UNIQUE(constructeur, designation)
 );
 
 CREATE TABLE Terminal (
 numero_serie 	VARCHAR(15) 		PRIMARY KEY,
-modèle 			INTEGER(4) 			REFERENCES Modele(id) NOT NULL,
-propriétaire 	INTEGER(4) 			REFERENCES Utilisateur(idClient) NOT NULL,
+modèle 			INTEGER				REFERENCES Modele(id) NOT NULL,
+propriétaire 	INTEGER				REFERENCES Utilisateur(idClient) NOT NULL
 );
 
 CREATE TABLE ProduitAchete (
-id 				INTEGER(2) 			PRIMARY KEY,
+id 				INTEGER				PRIMARY KEY,
 res 			VARCHAR(15) 		REFERENCES Ressource(nom),
 app 			VARCHAR(15) 		REFERENCES Application(nom),
-propriétaire 	INTEGER(4)			REFERENCES Utilisateur(idClient) NOT NULL,
+propriétaire 	INTEGER				REFERENCES Utilisateur(idClient) NOT NULL,
 CHECK(res IS NOT NULL OR app IS NOT NULL)
 );
 
 CREATE TABLE Installe_sur (
-produit 		INTEGER(2) 			REFERENCES ProduitAchete(id) PRIMARY KEY,
+produit 		INTEGER				REFERENCES ProduitAchete(id) PRIMARY KEY,
 terminal 		VARCHAR(10) 		REFERENCES Terminal(numero_serie)
 );
 
 CREATE TABLE Ressource_disponible_pour (
 res 			VARCHAR(15) 		REFERENCES Ressource(nom),
-systeme 		INTEGER(2) 			REFERENCES OS(id),
+systeme 		INTEGER				REFERENCES OS(id),
 PRIMARY KEY (res, systeme)
 );
 
 CREATE TABLE Application_disponible_pour (
 app 			VARCHAR(15) 		REFERENCES Application(nom),
-systeme 		INTEGER(2) 			REFERENCES OS(id),
+systeme 		INTEGER				REFERENCES OS(id),
 PRIMARY KEY (app, systeme)
 );
 
